207e99854c6acad81916441d7a6e46f9a86a59c7
iamwairimu <kmnleah@mail.com>
2023-06-15 14:00:32 -0700

    size on various type of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,11 @@
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
