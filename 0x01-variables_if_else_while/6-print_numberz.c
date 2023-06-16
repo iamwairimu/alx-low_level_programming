240a9bbd9a3298f6d59daa2fb28b6d3720be88dc
iamwairimu <kmnleah@mail.com>
2023-06-16 07:33:09 -0700

    numberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints single numberZ
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '10'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
