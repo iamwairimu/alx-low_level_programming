d2d5e7d2b72ff7a7a777562f260b672ae1409b10
iamwairimu <kmnleah@mail.com>
2023-06-20 09:45:21 -0700

    lowercase 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  * Return: 0 (success)
  */
-void print_alphabet_x10(void)...
+void print_alphabet_x10(void); ...
 {
 	int i;
 	int j;
