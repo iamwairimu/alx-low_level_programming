9a1d7df44f7e70f2ce129fc6617c0595f59ab759
iamwairimu <kmnleah@mail.com>
2023-06-20 14:02:50 -0700

    lowercase c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,13 @@
  */
 int _islower(int c)
 {
-	if (c == 97 && c < 122)
-    {
-        _putchar('\n');
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-    _putchar('\n');
+	if (c >= 97 && c < 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
