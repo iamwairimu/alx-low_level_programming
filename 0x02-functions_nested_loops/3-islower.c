6273a76f3e666dabe1a5638e867308a9a1caa622
iamwairimu <kmnleah@mail.com>
2023-06-20 13:53:31 -0700

    lowercase c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,16 @@
  * _islower - checks for lowercase character
  * Return: 0 (success)
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c == 97 && c < 122)
+    {
+        _putchar('\n');
+        return 1;
+    }
+    else
+    {
+	    _putchar('\n')
+        return 0;
+    }
 }
