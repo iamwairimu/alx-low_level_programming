b0f903fef60b4689a445d99d4c6a392f5e0689bd
iamwairimu <kmnleah@mail.com>
2023-06-20 15:37:43 -0700

    98 battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * Return: 0 (success)
+ */
+void print_to_98(int n)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
