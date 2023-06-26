5e61df62600d0f713202e1403c1c95d4c09291aa
iamwairimu <kmnleah@mail.com>
2023-06-26 07:25:29 -0700

    c pointer and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
-
-	while (*(src + 1) != '\0')
+	char *ptr = dest;
+	
+	while (src != '\0')
 	{
-		l++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	for ( ; x < l; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (ptr);
 }
