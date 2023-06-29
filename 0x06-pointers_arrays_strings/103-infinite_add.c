05e367dcab769b9dba7caae6eac0036cc46c2e64
iamwairimu <kmnleah@mail.com>
2023-06-29 07:26:18 -0700

    more pointers and arrays

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,19 +4,19 @@
  * @n: interger
  * Return: 0
  */
-void rev_string(char*n)
+void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
 	char temp;
 
-	while(*(n + i) != '\0')
+	while (*(n + i) != '\0')
 	{
 		i++;
 	}
 	i--;
 
-	for (j = 0; j < i; j++ , i--)
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
@@ -45,7 +45,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow ==1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
