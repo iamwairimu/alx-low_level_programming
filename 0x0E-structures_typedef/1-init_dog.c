2ab74331071e2ff619b3a2b78e422b089e22a680
iamwairimu <kmnleah@mail.com>
2023-07-17 11:02:11 -0700

    structures and typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "dog.h"
 /**
  * init_dog - initializes a variable of type struct dog
  * @d:struct dog pointer to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
+ * @name: to initialize
+ * @age: to initialize
+ * @owner: to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
