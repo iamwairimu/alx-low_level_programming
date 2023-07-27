a287083fb6ae7bf3540aca80422f3a1e0a414158
iamwairimu <kmnleah@mail.com>
2023-07-27 09:07:52 -0700

    changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,8 +18,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
-	return (*head);
+	return (new);
 }
