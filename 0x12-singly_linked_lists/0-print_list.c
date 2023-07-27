3df27c2646d9d1b8d0413909e0f1f42302772db7
iamwairimu <kmnleah@mail.com>
2023-07-27 10:29:31 -0700

    count

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
+
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
