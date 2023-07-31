28b1832c9b19bacb8bfaad4daf6e28ebfa6a062d
iamwairimu <kmnleah@mail.com>
2023-07-31 10:04:27 -0700

    rectify

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,8 +10,7 @@ void free_listint2(listint_t **head)
 	{
 		return;
 	}
-	listint_t *temp = (*head)->next;
+	free_listint2(&(*head))->next;
 	free(*head);
 	*head = NULL;
-	free_listint2(&temp);
 }
