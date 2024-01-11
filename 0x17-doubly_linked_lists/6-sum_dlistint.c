a7d26963fc644cfb3456f5e7de1f53e28e9ffe45
iamwairimu <kmnleah@gamil.com>
2024-01-11 04:48:31 -0800

    files

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of data in list
+ * @head: list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
