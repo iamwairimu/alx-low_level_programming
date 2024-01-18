be6414f72c9cb3f9e43e6350ea068dfeeb80c72b
iamwairimu <kmnleah@gamil.com>
2024-01-18 02:55:05 -0800

    downloading

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+wget -p .. https://raw.githubusercontent.com/iamwairimu/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.so
+export LD_PRELOAD="$PWD/../101-make_me_win.so"
